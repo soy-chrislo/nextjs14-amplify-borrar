2ff9c88fd7661b77047529105154b3a7a5f11ce7
chrislo <Green13-16l@hotmail.com>
2025-01-24 09:59:48 -0500

    enable storage integration 3

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -3,11 +3,11 @@ import { defineStorage } from "@aws-amplify/backend";
 export const storage = defineStorage({
 	name: "nextjs-app-files",
 	access: (allow) => ({
-		"/private-files/{entity_id}/*": [
+		"private-files/{entity_id}/*": [
 			allow.guest.to(["read"]),
 			allow.entity("identity").to(["read", "write", "delete"]),
 		],
-		"/public-files/*": [
+		"public-files/*": [
 			allow.authenticated.to(["read", "write"]),
 			allow.guest.to(["read", "write"]),
 		],
