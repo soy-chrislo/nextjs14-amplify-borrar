d9eba0fa36862d73673b82dd132c4195c337cd23
chrislo <green13-16l@hotmail.com>
2025-03-04 17:48:48 -0500

    rest api amplify gen 2 added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,17 @@
+import { Amplify } from 'aws-amplify';
+import { parseAmplifyConfig } from "aws-amplify/utils";
+import outputs from '@/../amplify_outputs.json';
+
+const amplifyConfig = parseAmplifyConfig(outputs);
+
+Amplify.configure({
+  ...amplifyConfig,
+  API: {
+    ...amplifyConfig.API,
+    REST: outputs.custom.API,
+  },
+});
+
 export default function Home() {
 	return (
 		<main>
